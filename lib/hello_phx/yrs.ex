deaf77f55c32b6bcefc084ec96a493068c3dc768
Michelle Tilley <michelle@michelletilley.net>
2024-09-27 10:47:58 -0700

    Add just a lil yrs

--- lib/hello_phx/yrs.ex
+++ lib/hello_phx/yrs.ex
@@ -3,5 +3,7 @@ defmodule HelloPhx.YRS do
     otp_app: :hello_phx,
     crate: :hellophx_yrs
 
-  def add(a, b), do: :erlang.nif_error(:nif_not_loaded)
+  def create_doc(), do: :erlang.nif_error(:nif_not_loaded)
+  def set_text(_doc, _text), do: :erlang.nif_error(:nif_not_loaded)
+  def get_text(_doc), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/hellophx_yrs/src/lib.rs
+++ native/hellophx_yrs/src/lib.rs
@@ -1,6 +1,49 @@
+use rustler::{Binary, Env, OwnedBinary, Resource, ResourceArc};
+use yrs::{Doc, GetString, Text, Transact};
+
+struct YrsWrapper {
+    doc: Doc,
+}
+
+impl YrsWrapper {
+    fn new() -> Self {
+        YrsWrapper { doc: Doc::new() }
+    }
+}
+
+#[rustler::resource_impl]
+impl Resource for YrsWrapper {}
+
 #[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
+fn create_doc() -> ResourceArc<YrsWrapper> {
+    ResourceArc::new(YrsWrapper::new())
+}
+
+#[rustler::nif]
+fn set_text(wrapper: ResourceArc<YrsWrapper>, bin: Binary) -> ResourceArc<YrsWrapper> {
+    {
+        let text = wrapper.doc.get_or_insert_text("test");
+        let mut txn = wrapper.doc.transact_mut();
+        let str = String::from_utf8(bin.to_vec()).expect("failed to convert bin to str");
+        text.insert(&mut txn, 0, &str);
+    }
+
+    wrapper
+}
+
+#[rustler::nif]
+fn get_text(env: Env, wrapper: ResourceArc<YrsWrapper>) -> Binary {
+    let text = wrapper.doc.get_or_insert_text("test");
+    let txn = wrapper.doc.transact_mut();
+    let str = text.get_string(&txn);
+    let str_sl = str.get(0..).unwrap().as_bytes();
+
+    let mut owned: rustler::OwnedBinary = OwnedBinary::new(str.len()).expect("Allocation failed");
+    let slice = owned.as_mut_slice();
+
+    slice.clone_from_slice(str_sl);
+
+    Binary::from_owned(owned, env)
 }
 
 rustler::init!("Elixir.HelloPhx.YRS");
