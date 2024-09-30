44479420e03ac9ddf4d702fc563109fb2b077408
Michelle Tilley <binarymuse@binarymuse.net>
2024-09-30 08:32:02 -0700

    Get basic BlockNotes running

--- config/config.exs
+++ config/config.exs
@@ -36,7 +36,7 @@ config :esbuild,
   version: "0.17.11",
   hello_phx: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2017 --loader:.js=jsx --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --external:*.woff --external:*.woff2),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
